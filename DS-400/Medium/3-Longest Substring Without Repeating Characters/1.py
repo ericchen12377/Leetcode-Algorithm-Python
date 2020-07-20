eaaefe7602cd931cc4cf908397c095128cc0a180
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-07-19 22:44:48 -0400

    update

---
+++ DS-400/Medium/3-Longest Substring Without Repeating Characters/1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def LongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = set()
+        for i in range(len(s)):
+            if s[i] not in res:
+                res.add(s[i])
+            else:
+                 
+
+
+        return answer.next
