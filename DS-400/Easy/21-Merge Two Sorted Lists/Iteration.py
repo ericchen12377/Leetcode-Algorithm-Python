0a39446ceec2f2fea93cd0665b26e882b57accc0
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-10-16 22:45:42 -0400

    Leetcode No.21

---
+++ DS-400/Easy/21-Merge Two Sorted Lists/Iteration.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        cur = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+        return dummy.next
