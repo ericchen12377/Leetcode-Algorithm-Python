1d7b47cf31535a9b047cd432efb28797fc23fc60
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-11-05 21:42:29 -0500

    Leetcode No.100

---
+++ DS-400/Easy/100-Same Tree/Iteration_Deque.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = deque([(p, q),])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                    
+        return True
