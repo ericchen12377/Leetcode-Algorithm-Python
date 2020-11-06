6e8652fb940a6cac2aa3bf241cf26ae823e0067d
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-11-05 21:29:54 -0500

    Leetcode No.99

---
+++ DS-400/Hard/99-Recover Binary Search Tree/Iteration.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        
+        stack = []
+        x = y = pred = None
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if pred and root.val < pred.val:
+                y = root
+                if x is None:
+                    x = pred
+                else:
+                    break
+            pred = root
+            root = root.right
+        x.val, y.val = y.val, x.val
+        return root
