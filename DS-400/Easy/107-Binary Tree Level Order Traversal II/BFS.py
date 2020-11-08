90d1ad3250842d3bb2e3e8fae6c6c47c6eebcec2
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-11-07 22:57:08 -0500

    Leetcode No.107

---
+++ DS-400/Easy/107-Binary Tree Level Order Traversal II/BFS.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        
+        levels = []
+        if not root:
+            return levels
+        
+        def BFS(node, level):
+            if len(levels) == level:
+                levels.append([])
+            levels[level].append(node.val)
+            
+            if node.left:
+                BFS(node.left, level + 1)
+            if node.right:
+                BFS(node.right, level + 1)
+        BFS(root, 0)
+        return reversed(levels)
+        
