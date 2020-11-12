f545f40a77e6ca5f29f6fdf283384a03a4a656bb
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-11-11 23:53:06 -0500

    Leetcode No.144

---
+++ DS-400/Medium/144-Binary Tree Preorder Traversal/Iteration_Stack.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        if not root:
+            return []
+        
+        stack, res = [root,], []
+        while stack:
+            root = stack.pop()
+            if root:
+                res.append(root.val)
+                if root.right:
+                    stack.append(root.right)
+                if root.left:
+                    stack.append(root.left)
+        return res
