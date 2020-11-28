f84aba2b8652643411068652f618e426c253c7e8
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-11-27 23:44:36 -0500

    Update Counter.py

--- 1stRound/Easy/409 Longest Palindrome/Counter.py
+++ 1stRound/Easy/409 Longest Palindrome/Counter.py
@@ -15,3 +15,24 @@ class Solution:
             else:
                 res += v  #add even
         return res + prime
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+            
+        dic = collections.Counter(s)
+        count = 0
+        check_odd = 0
+        for key in dic:
+            if dic[key] % 2 == 0:
+                count += dic[key]
+            elif dic[key] % 2 == 1:
+                count += dic[key] - 1
+                check_odd = 1
+        return count + check_odd
