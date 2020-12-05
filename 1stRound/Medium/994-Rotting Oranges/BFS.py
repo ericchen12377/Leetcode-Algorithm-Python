d7b3e0d8106aa3fc4d3a66a3ea6a03acb0898222
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-12-04 23:41:52 -0500

    Update BFS.py

--- 1stRound/Medium/994-Rotting Oranges/BFS.py
+++ 1stRound/Medium/994-Rotting Oranges/BFS.py
@@ -30,3 +30,68 @@ class Solution(object):
                       fresh -= 1
           d += 1
         return d-1 if fresh == 0 else -1
+
+
+
+class Solution(object):
+    def orangesRotting(self, matrix):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        #Sequential order
+        #-Count fresh oranges
+        #-put rotten oranges into queue
+        
+        #BFS:
+        #-use queue size to track minutes
+        #-process rotting oranges
+          #-rot adjacent fresh oranges
+          #-push into queue
+          #-decrement fresh oranges count
+            
+        directions = [[-1,0],
+                     [0,1],
+                     [1,0],
+                     [0,-1]]
+        rotten = 2
+        fresh = 1
+        empty = 0
+        
+        if len(matrix) == 0:
+            return 0
+        queue = []
+        freshoranges = 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == rotten:
+                    queue.append([row,col])
+                if matrix[row][col] == fresh:
+                    freshoranges += 1
+        curQueueSize = len(queue)
+        minutes = 0
+        while len(queue) > 0:
+            if curQueueSize == 0:
+                minutes += 1
+                curQueueSize = len(queue)
+            curOrange = queue.pop(0)
+            curQueueSize -= 1
+            row = curOrange[0]
+            col = curOrange[1]
+            
+            for i in range(len(directions)):
+                curDir = directions[i]
+                nextRow = curDir[0] + row
+                nextCol = curDir[1] + col
+                if nextRow < 0 or nextRow >= len(matrix) or nextCol < 0 or nextCol >= len(matrix[0]):
+                    continue
+                if matrix[nextRow][nextCol] == fresh:
+                    matrix[nextRow][nextCol] = 2
+                    freshoranges -= 1
+                    queue.append([nextRow, nextCol])
+        if freshoranges > 0:
+            return -1
+        return minutes
+            
+                    
+        
