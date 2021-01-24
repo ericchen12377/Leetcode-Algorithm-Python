abb23d5de1be6b8f2e02c13f0c2a322a01eadd61
Suiyao Chen <suiyaochen@mail.usf.edu>
2021-01-24 17:16:04 -0500

    Leetcode No.257

---
+++ DS-400/Easy/257-Binary Tree Paths/Iteration.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        res = []
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                res.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        return res
---
+++ DS-400/Easy/257-Binary Tree Paths/Recursion.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        paths = []
+
+        def helper(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path += '->'
+                    helper(root.left, path)
+                    helper(root.right, path)
+        helper(root, '')
+        return paths
